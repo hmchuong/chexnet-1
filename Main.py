0e4f2b3a6002d3cc35dff5159e24a1271531b130
chuonghuynh <minhchuong.itus@gmail.com>
2018-05-08 20:56:39 +0700

    update batch size

--- Main.py
+++ Main.py
@@ -42,7 +42,7 @@ def runTrain():
     nnClassCount = 14
 
     #---- Training settings: batch size, maximum number of epochs
-    trBatchSize = 128
+    trBatchSize = 200
     trMaxEpoch = 100
 
     #---- Parameters related to image transforms: size of the down-scaled image, cropped image
