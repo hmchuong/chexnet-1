3a2686b31a1e73435385c21717857ddacaca7b46
chuonghuynh <minhchuong.itus@gmail.com>
2018-05-16 17:26:19 +0700

    update batch size

--- Main.py
+++ Main.py
@@ -67,7 +67,7 @@ def runTest():
     nnArchitecture = 'DENSE-NET-121'
     nnIsTrained = True
     nnClassCount = 14
-    trBatchSize = 256
+    trBatchSize = 200
     imgtransResize = 256
     imgtransCrop = 224
 
